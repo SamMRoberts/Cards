4ec763f851459ff80de94ea3e70d4c2f4aa6c4d8
Sam Roberts <sam.m.roberts@outlook.com>
2021-03-17 17:49:44 -0400

    Created skeleton UI

---
+++ Blackjack/GameUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Blackjack
+{
+    public partial class GameUI : Form
+    {
+        public GameUI()
+        {
+            InitializeComponent();
+        }
+
+        private void GameUI_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Blackjack/Program.cs
+++ Blackjack/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BlackJack
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             Console.WriteLine("Game started.");
-            Environment environment = new Environment();
+            //Environment environment = new Environment();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Blackjack.GameUI());
         }
     }
 }
